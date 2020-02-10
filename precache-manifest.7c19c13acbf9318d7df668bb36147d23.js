53cd15cc8a74b4966859e423629265ac572c467d
Selim Pl <selosdz@gmail.com>
2020-02-10 15:39:39 +0100

    Updates

---
+++ precache-manifest.7c19c13acbf9318d7df668bb36147d23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a2567a46253b90bd0833c3f7fbcb3ea",
+    "url": "/React_list_to_do/index.html"
+  },
+  {
+    "revision": "cf0f8fec23fdf2d02cf5",
+    "url": "/React_list_to_do/static/css/main.144d2a73.chunk.css"
+  },
+  {
+    "revision": "efe5b4b2cd85e4968cd2",
+    "url": "/React_list_to_do/static/js/2.4c2e4bcb.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/React_list_to_do/static/js/2.4c2e4bcb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf0f8fec23fdf2d02cf5",
+    "url": "/React_list_to_do/static/js/main.4876d361.chunk.js"
+  },
+  {
+    "revision": "88deb355e2cd50ddf910",
+    "url": "/React_list_to_do/static/js/runtime-main.148c323a.js"
+  }
+]);
